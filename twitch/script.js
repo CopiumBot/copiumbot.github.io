e0767e1e9812fc4aaba190ace9ad8dfa8744335b
CopiumBot <patryk.mroczek33@wp.pl>
2026-02-13 12:45:09 +0100

    Added character replacement for the tts to work properly

--- twitch/script.js
+++ twitch/script.js
@@ -436,6 +436,20 @@ document.getElementById("notificationsButton").addEventListener("click", () =>
 	});
 });
 
+const ReplaceCharacters = (text) =>
+{
+	const map =
+	{
+		"a": "á", "c": "ć", "e": "é", "g": "ğ", "h": "ħ",
+    	"i": "í", "l": "ł", "n": "ń", "o": "ó", "r": "ř",
+    	"s": "ș", "t": "ť", "u": "ú", "w": "ŵ", "y": "ý", "z": "ź"
+	};
+
+	const regex = new RegExp(Object.keys(map).join("|"), "g");
+
+	return text.replace(regex, (matched) => map[matched]);
+}
+
 const AddToQueue = (username, message) =>
 {
 	if(config.ttsEnabled === false)
@@ -448,7 +462,7 @@ const AddToQueue = (username, message) =>
     utterance.volume = config.volume;
 	const currentVoice = userSettings.has(username) ? userSettings.get(username).voice : config.voice;
     utterance.voice = voices.find(voice => voice.name === currentVoice);
-    synth.speak(utterance);
+    synth.speak(ReplaceCharacters(utterance));
 }
 
 window.addEventListener("beforeunload", () =>
